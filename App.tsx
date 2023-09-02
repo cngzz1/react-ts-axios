97144f55edb63c3039951cfda9a6242f553f2fcc
cngzz1 <67855012+cngzz1@users.noreply.github.com>
2023-09-02 16:00:58 +0800

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,67 +1,91 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import React, { SetStateAction, useEffect, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 
-const UserComponent = () => {
-  // invoke useState on an empty array using array destructuring
-  const [users, setUsers]: [
-    User[],
-    React.Dispatch<React.SetStateAction<User[]>>
-  ] = useState<User[]>([]);
+function App() {
+  // 1
+  console.log('1 - Start here');
+  const UserComponent: () => JSX.Element = () => {
+    // invoke useState on an empty array using array destructuring
+    const [users, setUsers]: [
+      User[],
+      React.Dispatch<React.SetStateAction<User[]>>
+    ] = useState<User[]>([]);
+    // 2
+    console.log('2 - Get users from useState hook');
+    console.log(users);
+    // 3
+    console.log('3 - Get setUsers from useState hook');
+    console.log(useState);
 
-  // use the setUsers React.Dispatch<SetStateAction<User>> callback
-  // obtained from the useState React Hook
-  // We create a new callback function
-  // setResponseCallBack.
-  // This react callback is used for setting response.data for the User
-  // containing the data rendered in React Component.
-  const setResponseCallBack: (response: {
-    data: React.SetStateAction<User[]>;
-  }) => void = (response: { data: React.SetStateAction<User[]> }) => {
-    setUsers(response.data);
-  };
-  // Create a callback that invokes the axios API to first get the data
-  // then set the responseCallBack on the obtained response.
-  // Get with return a Promise<AxiosResponse<User>> instance.
-  const axiosGetCallback: () => void = async () => {
-    try {
-      axios
-        .get<User, AxiosResponse<SetStateAction<User[]>>>(
-          'https://jsonplaceholder.typicode.com/users'
-        )
-        .then<void, never>(setResponseCallBack);
-    } catch (error) {
-      // check if the error was thrown from axios
-      if (axios.isAxiosError(error)) {
-        // do something
-        // or just re-throw the error
-        throw error;
-      } else {
-        // do something else
-        // or creating a new error
-        throw new Error('different error than axios');
+    // use the setUsers React.Dispatch<SetStateAction<User>> callback
+    // obtained from the useState React Hook
+    // We create a new callback function
+    // setResponseCallBack.
+    // This react callback is used for setting response.data for the User
+    // containing the data rendered in React Component.
+
+    //7
+    const setResponseCallBack: (response: {
+      data: React.SetStateAction<User[]>;
+    }) => void = (response: { data: React.SetStateAction<User[]> }) => {
+      console.log('8 - inside setResponseCallBack to call setUsers');
+      setUsers(response.data);
+      console.log('10 - End of setUsers');
+    };
+
+    // Create a callback that invokes the axios API to first get the data
+    // then set the responseCallBack on the obtained response.
+
+    // Get with return a Promise<AxiosResponse<User>> instance.
+    // use the useEffect React hook to invoke the axios api callback
+    // on an empty list.
+    //4
+    console.log('4 callingUseEffect');
+    useEffect(() => {
+      try {
+        // 7
+        console.log('7 calling axios API using setResponseCallback');
+        axios
+          .get<User, AxiosResponse<SetStateAction<User[]>>>(
+            'https://jsonplaceholder.typicode.com/users'
+          )
+          .then<void, never>(setResponseCallBack);
+      } catch (error) {
+        // check if the error was thrown from axios
+        if (axios.isAxiosError(error)) {
+          // do something
+          // or just re-throw the error
+          throw error;
+        } else {
+          // do something else
+          // or creating a new error
+          throw new Error('different error than axios');
+        }
       }
+    }, []);
+    //5
+    console.log('5 - end of useEffect');
+
+    //useEffect is a React Hook that lets you synchronize a component with an external system.
+    // create a callback using the users list obtained from the useState
+    // React hook.
+    // We map each user instance from the users list
+    // and returns the corresponding JSX with user.name field.
+
+    // create the userMapper component which will be rendered
+    const userMapper: JSX.Element[] = users?.map<JSX.Element>((user: User) => {
+      console.log('9 - creating userMapper');
+      return (
+        <ul>
+          <li key={user?.id}>{user?.name}</li>
+        </ul>
+      );
+    });
+    if (userMapper) {
+      console.log('6 - return userMapper');
+      return <>{userMapper}</>;
     }
   };
-  // use the useEffect React hook to invoke the axios api callback
-  // on an empty list.
-  useEffect(axiosGetCallback, []);
-
-  //useEffect is a React Hook that lets you synchronize a component with an external system.
-  // create a callback using the users list obtained from the useState
-  // React hook.
-  // We map each user instance from the users list
-  // and returns the corresponding JSX with user.name field.
-  const userCallBack: (user: User) => JSX.Element = (user: User) => (
-    <ul>
-      <li>{user.name}</li>
-    </ul>
-  );
-
-  // create the userMapper component which will be rendered
-  const userMapper: JSX.Element[] = users.map<JSX.Element>(userCallBack);
-  return <>{userMapper}</>;
-};
-function App() {
   return <UserComponent />;
 }
 
